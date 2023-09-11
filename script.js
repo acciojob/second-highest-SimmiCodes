a058d48af7ab55334d6d24f93c57608d7ec7d41f
SimmiCodes <96693326+SimmiCodes@users.noreply.github.com>
2023-09-11 11:12:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+function secondHighest(arr) {
+  
+  if (arr.length <= 1) {
+    return -Infinity;
+  }
+const uniqueArr = [...new Set(arr)];
+
+ 
+  uniqueArr.sort((a, b) => b - a);
+
+  
+  if (uniqueArr.length < 2) {
+    return -Infinity;
+  }
+
+  
+  return uniqueArr[1];
+}
